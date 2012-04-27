5a77f87a61fcee99d93cd64dfae300576db59219
Al Wold <alwold@gmail.com>
2012-04-27 11:44:16 -0700

    initial work on node.js/express.js mock course schedule for testing

---
+++ routes/course.js
@@ -0,0 +1,16 @@
+exports.createCourse = function(req, res) {
+  courseMap = req.body.course;
+  course = new Course(courseMap.courseNumber, courseMap.name, courseMap.schedule, courseMap.status);
+  res.render('createCourse', { title: 'Create Course', course: course });
+};
+
+function Course(courseNumber, name, schedule, status) {
+  this.courseNumber = courseNumber;
+  this.name = name;
+  this.schedule = schedule;
+  this.status = status;
+}
+
+Course.prototype.toString = function() {
+  return "Course: ["+this.courseNumber+", "+this.name+", "+this.schedule+", "+this.status+"]";
+};
